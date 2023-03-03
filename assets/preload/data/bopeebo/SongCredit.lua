65dcb36927641797fa53bd9bf7acbad5857a3422
kaypogr <74113422+kypogr@users.noreply.github.com>
2023-03-02 19:12:58 -0800

    cumming rn

---
+++ assets/preload/data/bopeebo/SongCredit.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Original song by Kawai Sprite", 0, 900, 20);
+    setTextSize('watermark', 14);
+    addLuaText('watermark');
+end
---
+++ assets/preload/data/philly-nice/SongCredit.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Original song by Kawai Sprite", 0, 10, 680);
+    setTextSize('watermark', 14);
+    addLuaText('watermark');
+end
